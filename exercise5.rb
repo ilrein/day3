120033c9a2c801e5a3d7da53867c3f96086ab30a
Ilia <ilia.reingold@gmail.com>
2014-03-25 16:49:34 -0400

    fixed fahrenheit conversion with floats

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,7 @@
 def convert(temp)
-	c = ((temp - 32) * 5/9)	
+	c = ((temp - 32) * (5/9.to_f))	
 	return puts "#{c} is your fahrenheit temp into C"
 end
 puts "fahrenheit #?"
 temp = gets.chomp.to_i
-puts convert(temp)
+convert(temp)
